ea319a1b376726d0eae7af6870fe9ee78b2196f4
KaanAkay <78040963+KaanAkay@users.noreply.github.com>
2021-09-26 16:41:50 +0300

    Create BaseTest.java

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,32 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+import java.util.logging.Logger;
+
+public class BaseTest {
+
+    private static WebDriver driver;
+    private static final Logger logger = Logger.getLogger(BaseTest.class.getName());
+    private static final int DEFAULT_WAIT = 20;
+    private static final String driverName = "webdriver.chrome.driver";
+    private static final String googleDriverPath = "./driver/chromedriver";
+    private static final String baseUrl = "https://www.gittigidiyor.com/";
+    public static WebDriver getDriver() {return driver;}
+    public static int getDefaultWait(){return DEFAULT_WAIT;}
+
+    @Before
+    public void setup(){
+        driver.navigate().to(baseUrl);
+        logger.info("site Açılıyor.");
+
+    }
+
+    @After
+    public void tearDown(){
+       driver.quit();
+    }
+
+}
